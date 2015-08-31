76ada0b71b27f0108d2ed1d06b7e9e2f1df7f514
Christopher Wardell <github@cpwardell.com>
2015-08-31 16:48:19 +0900

    Basic version of function works.  No vignette yet.

--- R/barplot3d.R
+++ R/barplot3d.R
@@ -1,72 +1,43 @@
-## This script creates a "legoplot" similar to those produced by the Broad Institute
-## The plot shows the relative abundance of each of the 6 possible mutations in the 
-## 16 sequence contexts
+#' Barplot 3D
+#'
+#' Creates 3D barplots, with many configurable options. For detailed usage, please read the accompanying vignette.
+#' 
+#' @param z Numeric vector of the heights of each column
+#' @param dimensions  Numeric vector of length two; the number of rows and columns of the base of the plot
+#' @param scalexy     Numeric value used to scale the x and y coordinates (i.e. the "fatness" of the columns). Also scales the gap between columns
+#' @param scalez      Numeric value used to scale the z input vector. Useful for playing with data, but not recommended
+#' @param gap         Numeric value determining the gap between columns; this gap is identical in the x and y dimensions
+#' @param alpha       Numeric value determining the transparency of all column sides. Ranges beween 0 (invisible) and 1 (opaque)
+#' @param colors      Colour of columns.  Either a single colour or a character vector of colours, one for each column
+#' @param linecol     Colour of edges of columns. A single colour.
+#' @details Produces a 3D barplot using a numeric vector of values. The \code{\link{rgl}} package is used to draw the plots.
+#' Further details are available in the accompanying vignette.
+#' @return Draws 3D barplot; only the most recent object IDs will be returned.
+#' @export
+#' @import rgl
+#' @author Christopher Wardell \email{r@@cpwardell.com}
+#' @examples
+#' ## The vignette provides far more in-depth explanation and examples ##
+#' blah blah blah
 
-## Load packages
-library(rgl)
+## To do:
+# y and x axes
+# background mesh
+# size of plot
+# creation of png output
+# theta, phi and fov
+# theta, phi output?
+# Note: fills from left to right, front to back 
+# Check arguments are present and appropriate
 
-#### START OF FUNCTIONS
-
-## Functions modified from the "demo(hist3d)" examples in the rgl package:
-# library(rgl)
-# demo(hist3d)
-## Note; would it have killed the original author to comment their code?
-
-## Draws a single "column" or "stack".
-## X and Y coordinates determine the area of the column
-## The Z coordinate determines the height of the column
-## We include "lit=FALSE" arguments to remove the nasty shiny surfaces caused by lighting
-stackplot.3d<-function(x,y,z,alpha=1,topcol="#078E53",sidecol="#aaaaaa"){
-  
-  ## These lines allow the active rgl device to be updated with multiple changes
-  ## This is necessary to draw the sides and ends of the column separately  
-  save <- par3d(skipRedraw=TRUE)
-  on.exit(par3d(save))
-  
-  ## Determine the coordinates of each surface of the column and its edges
-  x1=c(rep(c(x[1],x[2],x[2],x[1]),3),rep(x[1],4),rep(x[2],4))
-  z1=c(rep(0,4),rep(c(0,0,z,z),4))
-  y1=c(y[1],y[1],y[2],y[2],rep(y[1],4),rep(y[2],4),rep(c(y[1],y[2],y[2],y[1]),2))
-  x2=c(rep(c(x[1],x[1],x[2],x[2]),2),rep(c(x[1],x[2],rep(x[1],3),rep(x[2],3)),2))
-  z2=c(rep(c(0,z),4),rep(0,8),rep(z,8) )
-  y2=c(rep(y[1],4),rep(y[2],4),rep(c(rep(y[1],3),rep(y[2],3),y[1],y[2]),2) )
-  
-  ## These lines create the sides of the column and its coloured top surface
-  rgl.quads(x1,z1,y1,col=rep(sidecol,each=4),alpha=alpha,lit=FALSE)
-  rgl.quads(c(x[1],x[2],x[2],x[1]),rep(z,4),c(y[1],y[1],y[2],y[2]),
-            col=rep(topcol,each=4),alpha=1,lit=FALSE) 
-  ## This line adds black edges to the column
-  rgl.lines(x2,z2,y2,col="#000000",lit=FALSE)
-}
-# Example:
-# stackplot.3d(c(0,1),c(0,1),3,alpha=0.6)
-
-## Calls stackplot.3d repeatedly to create a barplot
+## Calls singlecolumn repeatedly to create a barplot
 ## z is the heights of the columns and must be an appropriately named vector
-context3d<-function(z,alpha=1,scalexy=10,scalez=1,gap=0.2){
+barplot3d=function(z,dimensions,scalexy=1,scalez=1,gap=0.2,alpha=1,colors=NA,linecol="black"){
+  
   ## These lines allow the active rgl device to be updated with multiple changes
   ## This is necessary to add each column sequentially
   save <- par3d(skipRedraw=TRUE)
   on.exit(par3d(save))
-  
-  ## Recreate Broad order
-  types=c("C.G.G.C","T.A.A.T","C.A.G.T","T.G.A.C","C.T.G.A","T.C.A.G")
-  contexts=c("TxT","CxT","AxT","GxT","TxC","CxC","AxC","GxC",
-             "TxA","CxA","AxA","GxA","TxG","CxG","AxG","GxG")
-  typeorder=c()
-  for(type in types){
-    typeorder=c(typeorder,paste(type,contexts,sep="_"))
-  }
-  z=z[typeorder]
-  
-  ## Reorder data into 6 regions
-  set1=c(1:4,17:20,5:8,21:24,9:12,25:28,13:16,29:32)
-  set2=set1+32
-  set3=set1+64
-  neworder=c(set1,set2,set3)
-  
-  ## Define dimensions of the plot 
-  dimensions=c(12,8)
   
   ## Scale column area and the gap between columns 
   y=seq(1,dimensions[1])*scalexy
@@ -76,39 +47,49 @@ context3d<-function(z,alpha=1,scalexy=10,scalez=1,gap=0.2){
   ## Scale z coordinate
   z=z*scalez
   
-  ## Set up colour palette
-  broadcolors=c("#805D3F","#72549A","#5EAFB2","#3F4F9D","#F2EC3C","#74B655")
-  colors=as.vector(sapply(broadcolors,rep,16))
-  
+  ## Set up colour palette if no colours supplied in arguments
+  ## If no colour is supplied, the default is green
+  ## If a single colour is supplied it will be applied to all columns
+  if(length(colors)==1){
+    if(is.na(colors)){
+      colors=rep("#078E53",length(z))
+    }else{
+      colors=rep(color,length(z))
+    }
+  }
+    
   ## Plot each of the columns
   for(i in 1:dimensions[1]){
     for(j in 1:dimensions[2]){
       it=(i-1)*dimensions[2]+j # Variable to work out which column to plot; counts from 1:96
-      stackplot.3d(c(gap+x[j],x[j]+scalexy),
+      singlecolumn(c(gap+x[j],x[j]+scalexy),
                    c(-gap-y[i],-y[i]-scalexy),
-                   z[neworder[it]],
+                   z[it],
                    alpha=alpha,
-                   topcol=colors[neworder[it]],
-                   sidecol=colors[neworder[it]])
+                   topcol=colors[it],
+                   sidecol=colors[it],
+                   linecol=linecol)
     }
   }
   ## Set the viewpoint and add axes and labels
   rgl.viewpoint(theta=50,phi=40,fov=0)
-  axes3d("y-+",labels=TRUE)
+  axes3d("y-+",labels=TRUE,col="black")
 }
 # Example:
 # context3d(counts)
 
+
+
 #### END OF FUNCTIONS
 
 ## Read in example data and cast to an appropriate vector
-rawdata=read.table("snvspermegabase.txt",header=TRUE)
-counts=as.numeric(rawdata)
-names(counts)=colnames(rawdata)
+#rawdata=read.table("snvspermegabase.txt",header=TRUE)
+#counts=as.numeric(rawdata)
+#names(counts)=colnames(rawdata)
 
 ## Example plots
-context3d(counts)
-context3d(counts,alpha=0.4)
+#context3d(counts)
+#context3d(counts,alpha=0.4)
 
 ## Save your images to files if you wish
-rgl.snapshot(filename="example.png")
+#rgl.snapshot(filename="example.png")
---
+++ R/singlecolumn.R
@@ -0,0 +1,32 @@
+## Function modified from the "demo(hist3d)" examples in the rgl package:
+# library(rgl)
+# demo(hist3d)
+
+## Draws a single "column" or "stack".
+## X and Y coordinates determine the area of the column
+## The Z coordinate determines the height of the column
+## We include "lit=FALSE" arguments to remove the nasty shiny surfaces caused by lighting
+singlecolumn=function(x,y,z,alpha=1,topcol="#078E53",sidecol="#aaaaaa",linecol="#000000"){
+  
+  ## These lines allow the active rgl device to be updated with multiple changes
+  ## This is necessary to draw the sides and ends of the column separately  
+  save=par3d(skipRedraw=TRUE)
+  on.exit(par3d(save))
+  
+  ## Determine the coordinates of each surface of the column and its edges
+  x1=c(rep(c(x[1],x[2],x[2],x[1]),3),rep(x[1],4),rep(x[2],4))
+  z1=c(rep(0,4),rep(c(0,0,z,z),4))
+  y1=c(y[1],y[1],y[2],y[2],rep(y[1],4),rep(y[2],4),rep(c(y[1],y[2],y[2],y[1]),2))
+  x2=c(rep(c(x[1],x[1],x[2],x[2]),2),rep(c(x[1],x[2],rep(x[1],3),rep(x[2],3)),2))
+  z2=c(rep(c(0,z),4),rep(0,8),rep(z,8) )
+  y2=c(rep(y[1],4),rep(y[2],4),rep(c(rep(y[1],3),rep(y[2],3),y[1],y[2]),2) )
+  
+  ## These lines create the sides of the column and its coloured top surface
+  rgl.quads(x1,z1,y1,col=rep(sidecol,each=4),alpha=alpha,lit=FALSE)
+  rgl.quads(c(x[1],x[2],x[2],x[1]),rep(z,4),c(y[1],y[1],y[2],y[2]),
+            col=rep(topcol,each=4),alpha=1,lit=FALSE) 
+  ## This line adds black edges to the column
+  rgl.lines(x2,z2,y2,col=linecol,lit=FALSE)
+}
+# Example:
+# stackplot.3d(c(0,1),c(0,1),3,alpha=0.6)
